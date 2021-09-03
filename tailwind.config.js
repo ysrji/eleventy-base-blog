e876d93b693ceba9c7dfc07bfd992c9243b79f1d
ysrji <45195460+ysrji@users.noreply.github.com>
2021-09-03 17:00:56 +0800

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [
+    // './**/*.njk',
+  ],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
